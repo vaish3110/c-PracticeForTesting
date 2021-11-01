5b9f21b17b3ad1bf28cf5f9c3f0e3ad76a8e89f9
vaish3110 <93207986+vaish3110@users.noreply.github.com>
2021-11-01 20:11:10 +0530

    this file contains string program
    this file contains string program and their function.

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"what is u r name: ";
+    cin>>name;
+    cout<<"u r name is: "<<name<<endl;
+
+    return 0;
+}
